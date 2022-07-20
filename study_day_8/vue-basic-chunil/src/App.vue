3af4d8f62ac981e128333d2692a997291ea11b2b
BardsTale <civu@nate.com>
2022-07-20 21:51:35 +0900

    8일차 처음부터 새로 짜는 vue-cli 코드 중간 업로드

---
+++ study_day_8/vue-basic-chunil/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <hello-vue :mamaString="momsMsg"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>  -->
+</template>
+
+<script>
+import HelloVue from './components/HelloVue.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'hello-vue' : HelloVue
+  },
+  data:function(){
+    return {
+      momsMsg : 'hello little girl'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ study_day_8/vue-basic-chunil/src/components/HelloVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{myMsg}}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloVue',
+    props: {
+        //부모한테서 받은 프롭이름 : 값타입
+        mamaString : String
+    },
+    computed: {
+        myMsg: function () {
+            return `${this.mamaString}, Hello mommy`;
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
